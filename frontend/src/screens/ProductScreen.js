28ede161277a58657119b918c0437bff56b2ee6d
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2022-01-17 16:40:44 +0500

    first commit

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import Meta from '../components/Meta';
+import {
+  listProductDetails,
+  // createProductReview,
+} from '../actions/productActions';
+// import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
+
+const ProductScreen = ({ history, match }) => {
+  const dispatch = useDispatch();
+  const [qty, setQty] = useState(1);
+  const productId = match.params.id;
+
+  useEffect(() => {
+    dispatch(listProductDetails(productId));
+  }, [dispatch, match]);
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, product, error } = productDetails;
+
+  const formSubmitHandler = () => {
+    history.push(`/cart/${productId}?qty=${qty}`);
+  };
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt='product img' fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} Reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>Price :$ {product.price}</ListGroup.Item>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>$ {product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'Instock' : 'Outofstock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty:</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1}>{x + 1}</option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
+                <ListGroup.Item>
+                  <Row>
+                    <Button
+                      className='btn-block'
+                      type='submit'
+                      onClick={formSubmitHandler}
+                      disabled={product.countInStock === 0}
+                    >
+                      Add To Cart
+                    </Button>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default ProductScreen;
